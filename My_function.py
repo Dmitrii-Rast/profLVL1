d9fa5d1098802bdea1adacd81b5c804bdf6217d7
Dmitrii-Rast <dalive039@gmail.com>
2022-10-23 11:27:03 +0400

    Start_3

---
+++ My_function.py
@@ -0,0 +1,24 @@
+# def summa():
+#     a=5
+#     b=10
+#     c=a+b
+#     print(" СУММА",c)
+# summa()
+# def summa2(a,b):
+#     c=a+b
+#     print(" СУММА2",c)
+# summa2(500,400)
+def mult():
+    def dele1():
+        a=10
+        b=2
+        c=a/b
+        print("Result1",c)
+    dele1()
+    def dele2():
+        a=100
+        b=2
+        c=a/b
+        print("Result2",c)
+    dele2()
+mult()
